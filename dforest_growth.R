b2842aaabd2c5d84daee901219b2a613ebfbce3e
claudiapflores <cpflores@bren.ucsb.edu>
2021-05-10 12:09:18 -0700

    fixed description in function

--- dforest_growth.R
+++ dforest_growth.R
@@ -13,7 +13,7 @@ forest_growth = function(time, C, parms) {
   # compute rate of change of forest size
   dforest_growth = C * exp(parms$r*time)
   
-  # set rate of change to 0 if C is greater than carrying capacity (K)
+  # set rate of change to 0 if C is greater than carrying capacity (K) or for forest where carbon is  at or above the threshold canopy closure then set to g
   dforest_growth = ifelse(C = parms$K, 0, ifelse(C > parms$cc_threshold, parms$g, dforest_growth))
   return(list(dforest_growth))
 }
