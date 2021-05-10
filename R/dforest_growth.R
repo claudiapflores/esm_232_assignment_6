fee3ef5b29ee05cd1aa5bb7d8e2a55f0d0b7598f
bretfolger <bret@ucsb.edu>
2021-05-10 14:57:03 -0700

    changed growth rate equation and plotted graph 1

--- R/dforest_growth.R
+++ R/dforest_growth.R
@@ -11,10 +11,11 @@
 forest_growth = function(time, C, parms) {
   
   # compute rate of change of forest size
-  dforest_growth = C * exp(parms$r*time)
+  dforest_growth = C * parms$r 
   
   # set rate of change to 0 if C is greater than carrying capacity (K) or for forest where carbon is  at or above the threshold canopy closure then set to g
-  dforest_growth = ifelse(C = parms$K, 0, ifelse(C >= parms$cc_threshold, parms$g, dforest_growth))
+  dforest_growth = ifelse(C >= parms$K, 0, ifelse(C >= parms$cc_threshold, parms$g, dforest_growth))
+
   return(list(dforest_growth))
 }
 
